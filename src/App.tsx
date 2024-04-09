05d098be66826362539811f2fae29f06473af413
Sergio Azizi <sergioazizi@Sergios-MacBook-Pro.local>
2024-04-09 23:25:31 +0800

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const App = () => {
+  return <Wrapper>HELLO</Wrapper>;
+};
+
+const Wrapper = styled.div``;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+
+import { App } from "@src/App";
+
+const container = document.getElementById("container")!;
+const root = createRoot(container);
+
+root.render(<App />);
